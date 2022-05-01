aa57828f9f79d6f468c0aa96accf2dcdbfd80e2d
amtechaustria <info@amtech.at>
2022-05-01 23:27:24 +0200

    initial project

---
+++ src/test/java/amtech/petclinic/ApplicationTests.java
@@ -0,0 +1,13 @@
+package amtech.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
