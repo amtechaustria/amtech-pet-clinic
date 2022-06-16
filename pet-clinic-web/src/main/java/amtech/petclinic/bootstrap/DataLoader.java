1834ecf8e3dc81008eee53e953792022ae1eaa9e
amtechaustria <info@amtech.at>
2022-06-16 13:16:13 +0200

    Added PetTypes to bootstrap.  closes #32

--- pet-clinic-web/src/main/java/amtech/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/amtech/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package amtech.petclinic.bootstrap;
 
 import amtech.petclinic.model.Owner;
+import amtech.petclinic.model.PetType;
 import amtech.petclinic.model.Vet;
 import amtech.petclinic.services.OwnerService;
+import amtech.petclinic.services.PetTypeService;
 import amtech.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    
+    private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
